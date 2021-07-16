c0742679dffbfd5a0607f6c9e613d74aa4b4face
Shubham Tidke <shubham.tidke.165@gmail.com>
2021-07-17 01:09:03 +0530

    JS - OOPS

--- JS-Data Structures/oop.js
+++ JS-Data Structures/oop.js
@@ -81,3 +81,102 @@ Bird.prototype = {
     console.log("My name is " + this.name);
   }
 };
+
+//Inheritance
+function Animal() { };
+
+Animal.prototype = { //supertype[parent]
+  constructor: Animal,
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
+Bird.prototype = {
+  constructor: Bird
+};
+Dog.prototype = {
+  constructor: Dog
+};
+
+//Inherit Behaviors from a Supertype
+function Animal() { }
+
+Animal.prototype = {  //supertype
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+let duck = Object.create(Animal.prototype);
+duck instanceof Animal //true
+let beagle= Object.create(Animal.prototype);
+beagle.eat();//nom nom nom
+
+//setting new prototype using existing prototype
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+function Bird(){}
+Bird.prototype = Object.create(Animal.prototype);
+let crow = new Bird();
+crow.eat();
+
+// Add Methods After Inheritance
+function Animal() { }
+Animal.prototype.eat = function() {
+  console.log("nom nom nom");
+};
+function Bird() { }
+Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.constructor = Bird;
+Bird.prototype.fly = function() {
+  console.log("I'm flying!");
+};
+
+let beagle = new Bird();
+beagle.fly();
+
+//Override Inherited Methods
+function Animal() { }
+Animal.prototype.eat = function() {
+  return "nom nom nom";
+};
+function Bird() { }
+Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.eat = function() {
+  return "peck peck peck";
+};
+
+//Use a Mixin to Add Common Behavior Between Unrelated Objects
+/*
+there are cases when inheritance is not the best solution.
+Inheritance does not work well for unrelated objects like Bird and Airplane.
+They can both fly, but a Bird is not a type of Airplane and vice versa.
+For unrelated objects, it's better to use mixins. A mixin allows other objects to use a collection of functions.
+*/
+//common Property
+let flyMix = function(obj) {
+  obj.fly = function() {
+    console.log("Flying, wooosh!");
+  }
+};
+
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+
+let plane = {
+  model: "777",
+  numPassengers: 524
+};
+
+flyMix(bird); // bird using flyMixin
+flyMix(plane);//plane using flyMixin
+bird.fly();
+plane.fly();
