b4c50a5a718bd7e9f5a7a912d30406ad3990127a
Shubham Tidke <shubham.tidke.165@gmail.com>
2021-08-15 14:24:53 +0530

    Smallest common multiple

--- JS-Algorithm Scripting/scripting-4.js
+++ JS-Algorithm Scripting/scripting-4.js
@@ -99,7 +99,7 @@ sumFibs(4);
 //Sum of All Primes
 function sumPrimes(num) {
   let sum = 0;
-  for (var i = 2; i < num; i++) {
+  for (var i = 2; i <= num; i++) {
     if(isPrime(i)){
       sum+=i;
       console.log(i);
@@ -117,3 +117,32 @@ function isPrime(num){
 }
 
 sumPrimes(10);
+
+
+//Smallest Common Multiple
+/*
+Find the smallest common multiple of the provided parameters that can be evenly divided by both,
+as well as by all sequential numbers in the range between these parameters.
+*/
+function smallestCommons(arr) {
+  const [min,mix] = arr.sort((a,b)=> a-b);
+  const numberDivisors = max - min + 1;
+  let upperBound = 1 ;
+  for (let i = min; i <= max; i++) {
+    upperBound *= i;
+  }
+  for (let multiple = max; multiple <= upperBound; multiple += max) {
+    // Check if every value in range divides 'multiple'
+    let divisorCount = 0;
+    for (let i = min; i <= max; i++) {
+      // Count divisors
+      if (multiple % i === 0) {
+        divisorCount += 1;
+      }
+    }
+    if (divisorCount === numberDivisors) {
+      return multiple;
+    }
+  }
+}
+smallestCommons([1,5]);
