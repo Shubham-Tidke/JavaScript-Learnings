0d219b36094cab4f5a1cae65cc870cc45c5da957
Shubham Tidke <shubham.tidke.165@gmail.com>
2021-08-19 18:37:17 +0530

    Array Manipulation

--- JS-Algorithm Scripting/scripting-4.js
+++ JS-Algorithm Scripting/scripting-4.js
@@ -146,3 +146,22 @@ function smallestCommons(arr) {
   }
 }
 smallestCommons([1,5]);
+
+/*
+Given the array arr, iterate through and remove each element starting from the first element (the 0 index)
+until the function func returns true when the iterated element is passed through it.
+Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+*/
+function dropElements(arr, func) {
+  let originalLen = arr.length;
+  for (let i = 0; i < originalLen; i++) {
+    if (func(arr[0])) {
+      break;
+    } else {
+      arr.shift();
+    }
+  }
+  return arr;
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; });
